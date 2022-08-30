0977c37bdcc5e620486892084eddc247d172c1b5
Kerminator-dev <pussy_hunter@list.ru>
2022-08-30 08:19:44 +0300

    Изменена длина Username
    Изменена длина Username - от 3 до 20 символов

--- src/Chat/Chat.API/Models/Requests/RegisterRequest.cs
+++ src/Chat/Chat.API/Models/Requests/RegisterRequest.cs
@@ -5,8 +5,8 @@ namespace Chat.API.Models.Requests
     public class RegisterRequest
     {
         [Required(ErrorMessage = "Username is required!")]
-        [MinLength(4, ErrorMessage = "Min username length is 4 characters!")]
-        [MaxLength(10, ErrorMessage = "Max username length is 10 characters!")]
+        [MinLength(3, ErrorMessage = "Min username length is 3 characters!")]
+        [MaxLength(20, ErrorMessage = "Max username length is 20 characters!")]
         public string Username { get; set; }
 
         [Required(ErrorMessage = "Password is required!")]
